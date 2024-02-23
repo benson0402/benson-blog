99e0fa058ac95ca3420fc3b2a5f63c49a51ec046
benson0402 <52617098+benson0402@users.noreply.github.com>
2024-02-24 01:34:10 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Benson Wang`,
+      href: "https://github.com/benson0402",
     },
   ],
   // blog setting (required)
